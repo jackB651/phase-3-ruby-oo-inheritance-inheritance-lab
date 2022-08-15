04ac72fe2f24d265d8a44a755f9ff718eb442f2d
Jonathon <jwbialas@gmail.com>
2022-08-15 10:41:13 -0500

    teacher fin

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
-
+require_relative '../lib/user'
+class Student < User
+    attr_accessor :knowledge
+    def initilize 
+        @knowledge = []
+    end
+    def learn
+        @knowledge << Teacher 
+    end
+    def knowledge
+        @knowledge
+    end
 end
